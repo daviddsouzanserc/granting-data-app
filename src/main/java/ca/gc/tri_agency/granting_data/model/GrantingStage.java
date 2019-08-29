2de07fdc7566fe7ac8f1625b20062744d8aafae8
dev_dyd <dev_dyd@C14748.nserc.ca>
2019-08-29 14:54:56 -0400

    updated GrantingStage model for en and fr

--- src/main/java/ca/gc/tri_agency/granting_data/model/GrantingStage.java
+++ src/main/java/ca/gc/tri_agency/granting_data/model/GrantingStage.java
@@ -1,34 +1,59 @@
 package ca.gc.tri_agency.granting_data.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.springframework.context.i18n.LocaleContextHolder;
+
+import ca.gc.tri_agency.granting_data.model.util.LocalizedParametersModel;
+
 @Entity
-public class GrantingStage {
+public class GrantingStage implements LocalizedParametersModel {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
-	@Column(name = "granting_function")
-	private String grantingFunction;
+//	@Column(name = "granting_function")
+//	private String grantingFunction;
+
+	protected String nameEn;
+
+	protected String nameFr;
 
 	public GrantingStage() {
 
+	}
+
+	public String getName() {
+		String retval = "";
+		if (LocaleContextHolder.getLocale().toString().contains("en")) {
+			retval = getNameEn();
+		} else {
+			retval = getNameFr();
+		}
+		return retval;
+	}
+
+	public String getNameFr() {
+		return nameFr;
+	}
+
+	public String getNameEn() {
+		return nameEn;
 	}
 
 	public Long getId() {
 		return id;
 	}
 
-	public String getGrantingFunction() {
-		return grantingFunction;
+	public void setNameEn(String nameEn) {
+		this.nameEn = nameEn;
 	}
 
-	public void setGrantingFunction(String grantingFunction) {
-		this.grantingFunction = grantingFunction;
+	public void setNameFr(String nameFr) {
+		this.nameFr = nameFr;
 	}
 
 }

88d3760980bd6c957babdaaee776720095820001
YaserAbidakadr <yabid050@uottaw.ca>
2019-11-15 09:08:30 -0500

    Different color depending on the agency

--- src/main/java/ca/gc/tri_agency/granting_data/controller/SimpleEmailController.java
+++ src/main/java/ca/gc/tri_agency/granting_data/controller/SimpleEmailController.java
@@ -1,38 +1,45 @@
 package ca.gc.tri_agency.granting_data.controller;
 
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SimpleEmailController {
 
-//	@Autowired
-//	private JavaMailSender sender;
-//
-//	@RequestMapping("/viewFiscalYear")
-//	@ResponseBody
-//	String home() {
-//		try {
-//			sendEmail();
-//			return "Email Sent!";
-//		} catch (Exception ex) {
-//			return "Error in sending email: " + ex;
-//		}
-//		// UserRepo.getAllPersons().toString();
-//	}
-//
-//	private void sendEmail() throws Exception {
-//		try {
-//			MimeMessage message = sender.createMimeMessage();
-//			MimeMessageHelper helper = new MimeMessageHelper(message);
-//
-//			helper.setTo("yaserabidakadr@mailinator.com");
-//			helper.setText("How are you?");
-//			helper.setSubject("Hi");
-//
-//			sender.send(message);
-//
-//		} catch (Exception e) {
-//			System.out.println(e);
-//		}
-//	}
+	@Autowired
+	private JavaMailSender sender;
+
+	@RequestMapping("/viewFy")
+	@ResponseBody
+	String home() {
+		try {
+			sendEmail();
+			return "Email Sent!";
+		} catch (Exception ex) {
+			return "Error in sending email: " + ex;
+		}
+		// UserRepo.getAllPersons().toString();
+	}
+
+	private void sendEmail() throws Exception {
+		try {
+			MimeMessage message = sender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(message);
+
+			helper.setTo("yaserabidakadr@mailinator.com");
+			helper.setText("How are you?");
+			helper.setSubject("Hi");
+
+			sender.send(message);
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }

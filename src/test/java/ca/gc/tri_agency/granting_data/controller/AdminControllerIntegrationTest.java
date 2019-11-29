c123d3678202f50a73b6c5131b021c3cfa1fe6f7
DEV_JFS <DEV_JFS@C16573.nserc.ca>
2019-11-29 11:43:50 -0500

    fixed test that verifies redirect missing Role page (looks for managedError)

--- src/test/java/ca/gc/tri_agency/granting_data/controller/AdminControllerIntegrationTest.java
+++ src/test/java/ca/gc/tri_agency/granting_data/controller/AdminControllerIntegrationTest.java
@@ -1,7 +1,9 @@
 package ca.gc.tri_agency.granting_data.controller;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
@@ -40,10 +42,11 @@ public class AdminControllerIntegrationTest {
 				.andExpect(status().is3xxRedirection());
 	}
 
-	// @WithMockUser(username = "nserc-user", roles = { "SSHRC" })
-	// @Test
-	public void givenSshrcRequestOnAdminUrl_shouldFailWith401() throws Exception {
-		mvc.perform(get("/admin/home").contentType(MediaType.APPLICATION_XHTML_XML)).andExpect(status().isForbidden());
+	@WithMockUser(username = "nserc-user", roles = { "SSHRC" })
+	@Test
+	public void givenSshrcRequestOnAdminUrl_shouldFailWithManagedError() throws Exception {
+		mvc.perform(get("/admin/home").contentType(MediaType.APPLICATION_XHTML_XML))
+				.andExpect(content().string(containsString("managedError")));
 	}
 
 	@WithMockUser(username = "admin", roles = { "MDM ADMIN" })

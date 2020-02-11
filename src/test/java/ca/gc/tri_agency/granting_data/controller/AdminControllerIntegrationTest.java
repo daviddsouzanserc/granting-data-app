36fc1763c1d386f21748386977eae67aa0946e42
dev_aha <Alexander.Hamilton@NSERC-CRSNG.gc.ca>
2020-02-11 14:44:42 -0500

    Deliverable 18431 can be reviewed

--- src/test/java/ca/gc/tri_agency/granting_data/controller/AdminControllerIntegrationTest.java
+++ src/test/java/ca/gc/tri_agency/granting_data/controller/AdminControllerIntegrationTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Before;
@@ -12,10 +13,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -36,6 +39,46 @@ public class AdminControllerIntegrationTest {
 		mvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
 	}
 
+	@WithMockUser(roles = { "NSERC_USER", "SSHRC_USER", "AGENCY_USER" })
+	@Test
+	public void test_nonAdminUserCannotAddFundingOpportunities_shouldRedirectToLoginWith302() throws Exception {
+		mvc.perform(get("/manage/addFo").param("id", "26").param("nameEn", "A").param("nameFr", "B")
+				.param("leadAgency", "3").param("division", "Q").param("isJointIntiative", "false")
+				.param("_isJointIntiative", "on").param("partnerOrg", "Z").param("isComplex", "false")
+				.param("_isComplex", "on").param("isEdiRequired", "false").param("_isEdiRequired", "on")
+				.param("fundingType", "E").param("frequency", "Once").param("applyMethod", "NOLS")
+				.param("awardManagementSystem", "SSHERC").param("isNOI", "false").param("_isNOI", "on")
+				.param("isLOI", "false").param("_isLOI", "on")).andExpect(status().isOk())
+				.andExpect(content().string(containsString("id=\"forbiddenByRoleErrorPage\"")));
+	}
+
+	@WithAnonymousUser
+	@Test
+	public void test_anonUserCannotAddFundingOpportunities_shouldRedirectToLoginWith302() throws Exception {
+		mvc.perform(get("/manage/addFo").param("id", "26").param("nameEn", "A").param("nameFr", "B")
+				.param("leadAgency", "3").param("division", "Q").param("isJointIntiative", "false")
+				.param("_isJointIntiative", "on").param("partnerOrg", "Z").param("isComplex", "false")
+				.param("_isComplex", "on").param("isEdiRequired", "false").param("_isEdiRequired", "on")
+				.param("fundingType", "E").param("frequency", "Once").param("applyMethod", "NOLS")
+				.param("awardManagementSystem", "SSHERC").param("isNOI", "false").param("_isNOI", "on")
+				.param("isLOI", "false").param("_isLOI", "on")).andExpect(status().isFound())
+				.andExpect(redirectedUrl("http://localhost/login"));
+	}
+
+	@WithMockUser(roles = { "MDM ADMIN" })
+	@Test
+	public void test_onlyAdminCanAddFundingOpportunities_shouldSucceedWith302() throws Exception {
+		mvc.perform(get("/manage/addFo").param("id", "26").param("nameEn", "A").param("nameFr", "B")
+				.param("leadAgency", "3").param("division", "Q").param("isJointIntiative", "false")
+				.param("_isJointIntiative", "on").param("partnerOrg", "Z").param("isComplex", "false")
+				.param("_isComplex", "on").param("isEdiRequired", "false").param("_isEdiRequired", "on")
+				.param("fundingType", "E").param("frequency", "Once").param("applyMethod", "NOLS")
+				.param("awardManagementSystem", "SSHERC").param("isNOI", "false").param("_isNOI", "on")
+				.param("isLOI", "false").param("_isLOI", "on")).andExpect(status().isFound())
+				.andExpect(MockMvcResultMatchers.redirectedUrl("/browse/goldenList"));
+	}
+
+	@WithAnonymousUser
 	@Test
 	public void givenAnonymousRequestOnAdminUrl_shouldFailWith301() throws Exception {
 		mvc.perform(get("/admin/home").contentType(MediaType.APPLICATION_XHTML_XML))

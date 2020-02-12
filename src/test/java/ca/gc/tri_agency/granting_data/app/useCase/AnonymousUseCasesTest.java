908beb142e160922d446729b2d1eec9c0bff1aa4
dev_aha <Alexander.Hamilton@NSERC-CRSNG.gc.ca>
2020-02-12 10:47:48 -0500

    Deliverable 18437 is ready to be reviewed

---
+++ src/test/java/ca/gc/tri_agency/granting_data/app/useCase/AnonymousUseCasesTest.java
@@ -0,0 +1,56 @@
+package ca.gc.tri_agency.granting_data.app.useCase;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import ca.gc.tri_agency.granting_data.app.GrantingDataApp;
+import ca.gc.tri_agency.granting_data.repo.FundingOpportunityRepository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = GrantingDataApp.class)
+@ActiveProfiles("test")
+public class AnonymousUseCasesTest {
+
+	@Autowired
+	private WebApplicationContext ctx;
+	@Autowired
+	private FundingOpportunityRepository foRepo;
+
+	private MockMvc mvc;
+
+	@Before
+	public void setup() {
+		mvc = MockMvcBuilders.webAppContextSetup(ctx).apply(SecurityMockMvcConfigurers.springSecurity()).build();
+	}
+
+	@WithAnonymousUser
+	@Test
+	public void test_anonUserCanAccessViewFoPage_shouldSucceedWith200() throws Exception {
+		String foId = Long.toString(foRepo.findAll().get(0).getId());
+		mvc.perform(get("/browse/viewFo").param("id", foId)).andExpect(status().isOk())
+				.andExpect(content().string(containsString("id=\"/browse/viewFoPage\"")));
+	}
+
+	@WithAnonymousUser
+	@Test
+	public void test_anonUserCanAccessViewGoldenListPage_shouldSucceedWith200() throws Exception {
+		mvc.perform(get("/browse/goldenList")).andExpect(status().isOk())
+				.andExpect(content().string(containsString("id=\"/browse/goldenListPage\"")));
+	}
+
+}

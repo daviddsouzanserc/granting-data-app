ee3d9c49fb9358938674534c358f201b32bfc7dd
dev_aha <Alexander.Hamilton@NSERC-CRSNG.gc.ca>
2020-02-12 13:27:12 -0500

    Deliverable 18440 is ready to be reviewed

--- src/test/java/ca/gc/tri_agency/granting_data/app/useCase/AnonymousUseCasesTest.java
+++ src/test/java/ca/gc/tri_agency/granting_data/app/useCase/AnonymousUseCasesTest.java
@@ -15,6 +15,7 @@ import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfig
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -31,17 +32,26 @@ public class AnonymousUseCasesTest {
 	@Autowired
 	private FundingOpportunityRepository foRepo;
 
+	private String foId;
 	private MockMvc mvc;
 
 	@Before
 	public void setup() {
 		mvc = MockMvcBuilders.webAppContextSetup(ctx).apply(SecurityMockMvcConfigurers.springSecurity()).build();
+		foId = Long.toString(foRepo.findAll().get(0).getId());
+	}
+
+	@WithAnonymousUser
+	@Test
+	public void test_anonUserCanViewFoPageInFrench_shouldSucceed200() throws Exception {
+		mvc.perform(get("/browse/viewFo").param("id", foId).param("lang", "fr")).andExpect(status().isOk())
+				.andExpect(content().string(containsString("id=\"/browse/viewFoPage\"")))
+				.andExpect(content().string(containsString("lang=\"fr\""))).andDo(MockMvcResultHandlers.print());
 	}
 
 	@WithAnonymousUser
 	@Test
 	public void test_anonUserCanAccessViewFoPage_shouldSucceedWith200() throws Exception {
-		String foId = Long.toString(foRepo.findAll().get(0).getId());
 		mvc.perform(get("/browse/viewFo").param("id", foId)).andExpect(status().isOk())
 				.andExpect(content().string(containsString("id=\"/browse/viewFoPage\"")));
 	}

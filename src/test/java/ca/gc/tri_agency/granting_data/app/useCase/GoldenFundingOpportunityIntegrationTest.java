4e1c4a4c63d36fe1a8f2badcfa77a85e1d00f149
Alex Hamilton <ajhamilton15@hotmail.com>
2020-03-17 10:51:52 -0400

    Added createFO page and test class

---
+++ src/test/java/ca/gc/tri_agency/granting_data/app/useCase/GoldenFundingOpportunityIntegrationTest.java
@@ -0,0 +1,9 @@
+package ca.gc.tri_agency.granting_data.app.useCase;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class GoldenFundingOpportunityIntegrationTest {
+
+}

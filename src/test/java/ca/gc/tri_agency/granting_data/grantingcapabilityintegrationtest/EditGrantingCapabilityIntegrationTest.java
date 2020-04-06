f497c190a7942c44314efb1385f8944c1f201b71
Alex Hamilton <ajhamilton15@hotmail.com>
2020-04-06 14:33:03 -0400

    Completed controller integration tests for GrantingCapability

--- src/test/java/ca/gc/tri_agency/granting_data/grantingcapabilityintegrationtest/EditGrantingCapabilityIntegrationTest.java
+++ src/test/java/ca/gc/tri_agency/granting_data/grantingcapabilityintegrationtest/EditGrantingCapabilityIntegrationTest.java
@@ -6,8 +6,9 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.hamcrest.Matchers;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import ca.gc.tri_agency.granting_data.app.GrantingDataApp;
+import ca.gc.tri_agency.granting_data.model.GrantingCapability;
+import ca.gc.tri_agency.granting_data.repo.GrantingCapabilityRepository;
 import ca.gc.tri_agency.granting_data.service.GrantingCapabilityService;
 
 @SpringBootTest(classes = GrantingDataApp.class)
@@ -35,7 +38,9 @@ public class EditGrantingCapabilityIntegrationTest {
 	private WebApplicationContext ctx;
 
 	@Autowired
-	private GrantingCapabilityService gsService;
+	private GrantingCapabilityService gcService;
+	@Autowired
+	private GrantingCapabilityRepository gcRepo;
 
 	private MockMvc mvc;
 
@@ -47,72 +52,131 @@ public class EditGrantingCapabilityIntegrationTest {
 	@WithMockUser(username = "admin", roles = { "MDM ADMIN" })
 	@Test
 	public void test_editGCLinkVisibleToAdmin_shouldSucceedWith200() throws Exception {
-		long numGCs = gsService.findGrantingCapabilitiesByFoId(1L).size();
+		long numGCs = gcService.findGrantingCapabilitiesByFoId(1L).size();
 		assertTrue(numGCs > 0);
 
 		String response = mvc.perform(MockMvcRequestBuilders.get("/manage/manageFo").param("id", "1"))
 				.andExpect(MockMvcResultMatchers.status().isOk()).andReturn().getResponse().getContentAsString();
 		Pattern regex = Pattern.compile("\n");
 		long numEditGCLinks = regex.splitAsStream(response).filter(line -> line.contains("href=\"editGC?id=")).count();
-		
+
 		assertEquals(numGCs, numEditGCLinks);
 	}
-	
+
 	@WithMockUser(username = "admin", roles = { "NSERC_USER", "SSHRC_USER", "AGENCY_USER" })
 	@Test
 	public void test_editGCLinkNotVisibleToNonAdmin_shouldReturn200() throws Exception {
-		long numGCs = gsService.findGrantingCapabilitiesByFoId(1L).size();
+		long numGCs = gcService.findGrantingCapabilitiesByFoId(1L).size();
 		assertTrue(numGCs > 0);
 
 		String response = mvc.perform(MockMvcRequestBuilders.get("/manage/manageFo").param("id", "1"))
 				.andExpect(MockMvcResultMatchers.status().isOk()).andReturn().getResponse().getContentAsString();
 		Pattern regex = Pattern.compile("\n");
 		long numEditGCLinks = regex.splitAsStream(response).filter(line -> line.contains("href=\"editGC?id=")).count();
-		
+
 		assertNotEquals(numGCs, numEditGCLinks);
 		assertEquals(0, numEditGCLinks);
 	}
 
-	@Ignore
-	@WithMockUser(username = "admin", roles = { "MDM ADMIN" })
-	@Test
-	public void test_adminCanAccessEditGCPage_shouldSucceedWith200() throws Exception {
-		// TODO: COMPLETE DEFINITION
-	}
-
-	@Ignore
-	@WithMockUser(roles = { "NSERC_USER", "SSHRC_USER", "AGENCY_USER" })
-	@Test
-	public void test_nonAdminCannotAccessEditGCPage_shouldReturn403() throws Exception {
-		// TODO: COMPLETE DEFINITION
-	}
-
-	@Ignore
 	@WithMockUser(username = "admin", roles = { "MDM ADMIN" })
 	@Test
 	public void testService_adminCanEditGC() {
-		// TODO: COMPLETE DEFINITION
+		long initGcRepoCount = gcRepo.count();
+
+		GrantingCapability gc = gcService.findGrantingCapabilityById(1L);
+		String editDescription = "TEST DESCRIPTION EDIT";
+		gc.setDescription(editDescription);
+		gcService.saveGrantingCapability(gc);
+
+		assertTrue(gcService.findGrantingCapabilityById(1L).getDescription().equals(editDescription));
+		assertEquals(initGcRepoCount, gcRepo.count());
 	}
 
-	@Ignore
 	@WithMockUser(roles = { "NSERC_USER", "SSHRC_USER", "AGENCY_USER" })
 	@Test(expected = AccessDeniedException.class)
 	public void testService_nonAdminCannotEditGC_shouldThrowAccessDeniedException() {
-		// TODO: COMPLETE DEFINITION
+		GrantingCapability gc = gcService.findGrantingCapabilityById(1L);
+		String editDescription = "TEST DESCRIPTION EDIT";
+		gc.setDescription(editDescription);
+		gcService.saveGrantingCapability(gc);
 	}
 
-	@Ignore
+	@WithMockUser(username = "admin", roles = { "MDM ADMIN" })
+	@Test
+	public void test_adminCanAccessEditGCPage_shouldSucceedWith200() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/manage/editGC").param("id", "1"))
+				.andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content()
+						.string(Matchers.containsString("id=\"editGrantingCapabilityPage\"")));
+	}
+
+	@WithMockUser(roles = { "NSERC_USER", "SSHRC_USER", "AGENCY_USER" })
+	@Test
+	public void test_nonAdminCannotAccessEditGCPage_shouldReturn403() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/manage/editGC").param("id", "1"))
+				.andExpect(MockMvcResultMatchers.status().isForbidden()).andExpect(MockMvcResultMatchers.content()
+						.string(Matchers.containsString("id=\"forbiddenByRoleErrorPage\"")));
+	}
+
 	@WithMockUser(username = "admin", roles = { "MDM ADMIN" })
 	@Test
 	public void testController_adminCanEditGC_shouldSucceedWith302() throws Exception {
-		// TODO: COMPLETE DEFINITION
+		long initGcRepoCount = gcRepo.count();
+		GrantingCapability gcBeforeUpdate = gcService.findGrantingCapabilityById(1L);
+
+		String description = RandomStringUtils.randomAlphabetic(10);
+		String url = "www" + RandomStringUtils.randomAlphabetic(10) + ".ca";
+		String foId = "1";
+		String gStageId = "1";
+		String gSystemId = "1";
+
+		mvc.perform(MockMvcRequestBuilders.post("/manage/editGC").param("id", "1").param("description", description)
+				.param("foId", foId).param("gStageId", gStageId).param("gSystemId", gSystemId))
+				.andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+				.andExpect(MockMvcResultMatchers.redirectedUrl("/manage/manageFo?id=" + foId))
+				.andExpect(MockMvcResultMatchers.flash().attribute("actionMsg", "Edited a Granting Capability"));
+
+		mvc.perform(MockMvcRequestBuilders.get("/manage/manageFo").param("id", foId))
+				.andExpect(MockMvcResultMatchers.flash().attributeCount(0));
+
+		GrantingCapability gcAfterUpdate = gcService.findGrantingCapabilityById(1L);
+
+		assertNotEquals(gcBeforeUpdate, gcAfterUpdate);
+		assertEquals(description, gcAfterUpdate.getDescription());
+		assertEquals(url, gcAfterUpdate.getUrl());
+		assertEquals(foId, gcAfterUpdate.getFundingOpportunity().getId());
+		assertEquals(gStageId, gcAfterUpdate.getGrantingStage().getId());
+		assertEquals(gSystemId, gcAfterUpdate.getGrantingSystem().getId());
+
+		assertEquals(initGcRepoCount, gcRepo.count());
 	}
 
-	@Ignore
 	@WithMockUser(roles = { "NSERC_USER", "SSHRC_USER", "AGENCY_USER" })
 	@Test
 	public void testController_nonAdminCannotEditGC_shouldReturn403() throws Exception {
-		// TODO: COMPLETE DEFINITION
+		long initGcRepoCount = gcRepo.count();
+		GrantingCapability gcBeforeFailedUpdate = gcService.findGrantingCapabilityById(1L);
+
+		String description = RandomStringUtils.randomAlphabetic(10);
+		String url = "www" + RandomStringUtils.randomAlphabetic(10) + ".ca";
+		String foId = "1";
+		String gStageId = "1";
+		String gSystemId = "1";
+
+		mvc.perform(MockMvcRequestBuilders.post("/manage/editGC").param("id", "1").param("description", description)
+				.param("foId", foId).param("gStageId", gStageId).param("gSystemId", gSystemId))
+				.andExpect(MockMvcResultMatchers.status().isForbidden()).andExpect(MockMvcResultMatchers.content()
+						.string(Matchers.containsString("id=\"forbiddenByRoleErrorPage\"")));
+
+		GrantingCapability gcAfterFailedUpdate = gcService.findGrantingCapabilityById(1L);
+
+		assertEquals(gcBeforeFailedUpdate, gcAfterFailedUpdate);
+		assertNotEquals(description, gcAfterFailedUpdate.getDescription());
+		assertNotEquals(url, gcAfterFailedUpdate.getUrl());
+		assertNotEquals(foId, gcAfterFailedUpdate.getFundingOpportunity().getId());
+		assertNotEquals(gStageId, gcAfterFailedUpdate.getGrantingStage().getId());
+		assertNotEquals(gSystemId, gcAfterFailedUpdate.getGrantingSystem().getId());
+
+		assertEquals(initGcRepoCount, gcRepo.count());
 	}
 
 }
